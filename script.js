9a5c15cbaec40318fc82b126f219f6c942e5bd72
griiig <57357544+griiig@users.noreply.github.com>
2020-09-30 12:01:24 +0200

    layout
    jeg har lavet et GUI

---
+++ script.js
@@ -0,0 +1,70 @@
+result = document.getElementById("result");
+
+function a(){
+    console.log("7")
+    result.innerHTML += "7";
+}
+function b(){
+    console.log("8")
+    result.innerHTML += "8";
+}
+function c(){
+    console.log("9")
+    result.innerHTML += "9";
+}
+function d(){
+    console.log("*")
+    result.innerHTML += "*";
+}
+function e(){
+    console.log("4")
+    result.innerHTML += "4";
+}
+function f(){
+    console.log("5")
+    result.innerHTML += "5";    
+}
+function g(){
+    console.log("6")
+    result.innerHTML += "6";
+}
+function h(){
+    console.log(":")
+    result.innerHTML += ":";
+}
+function i(){
+    console.log("1")
+    result.innerHTML += "1";
+}
+function j(){
+    console.log("2")
+    result.innerHTML += "2";
+}
+function k(){
+    console.log("3")
+    result.innerHTML += "3";
+}
+function l(){
+    console.log("+")
+    result.innerHTML += "+";
+}
+function m(){
+    console.log("0")
+    result.innerHTML += "0";
+}
+function n(){
+    console.log(",")
+    result.innerHTML += ",";
+}
+function o(){
+    console.log("-")
+    result.innerHTML += "-";
+}
+function p(){
+    console.log("=")
+    result.innerHTML += "=";
+}
+function z(){
+    console.log("c")
+    result.innerHTML = "";
+}
